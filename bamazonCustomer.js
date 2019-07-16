c6a928d98bca48c88032d143858eb5d0c674efb6
Mohamed Ali <mohamedkosol@gmail.com>
2019-07-16 17:06:03 -0500

    Bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,41 +29,76 @@ function start() {
    connection.query("SELECT * FROM products", function(err, res) {
        if (err) throw err;
        console.table(res);
-   })
-   // function purchasePrompt(){
+      //  console.log(Object.values(res));
+
+    
+        var choiceArray = [];
+        for (var i = 0; i < res.length; i++) {
+          choiceArray.push(res[i].item_id);
+        }
+      
+      
+
+
+
   inquirer.prompt([
      {
-       name: "items_id",
-       type: "input",
-       message: "What product would you like to buy?"
-
-     },
+      type: "rawlist",
+       name: "id",
+       message: "What product would you like to buy?",
+       choices: ["1", "2", '3', '4', '5', '6', '7', '8', '9', '10']
+     
+      },
      {
+      type: "input",
        name: "quantity",
-       type: "input",
        message: "How many units of this stock quantity would you like to buy?"
      }
      ])
    .then(function(answers){
-       var quantityNeeded = answers.Quantity;
-       var IDrequested = answers.ID;
-       purchaseOrder(IDrequested, quantityNeeded);
+     return purchaseOrder(answers.id, answers.quantity);
+   }).then(function(result){
+    
+    updateOrder(result.id, result.updatedStock, result.price,result.productName, result.totalCost);
+   })
+   .catch(function(err){
+     console.log(err);
    });
-  };
+  });
+}
 
   function purchaseOrder(ID, amtNeeded){
-      connection.query('Select * FROM products WHERE items_id = ' + ID, function(err,res){
-          if(err){console.log(err)};
-          if(amtNeeded <= res[i].stock_quantity){
-             // var totalCost = res[0].price * amtNeeded;
-              console.log("Good news your order is in stock!");
-              console.log("Your total cost for " + amtNeeded + " " +res[i].product_name + " is " + totalCost + " Thank you!");
+    return new Promise(function(resolve, reject){
+      connection.query('Select * FROM products WHERE ?', {items_id: ID}, function(err,res){
+        console.log(res);
+        if(err) throw err;
+        if(amtNeeded <= res[0].stock){
+          resolve({
+            id: ID,
+            totalCost: res[0].price * amtNeeded,
+            updatedStock: res[0].stock - amtNeeded,
+            price: res[0].price,
+            productName: res[0].departments_name
+          });
+        } else{
+          console.log("Insufficient quantity, sorry we do not have enough " + res[0].products_name + " to complete your order.");
+          reject({
+            error: 'you goofed'
+          });
+        }
+      })
+    });
+  }
 
-              connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
-          } else{
-              console.log("Insufficient quantity, sorry we do not have enough " + res[i].product_name + "to complete your order.");
-          };
-       //    displayProducts();
+  function updateOrder(ID, updatedStock, price, productName, totalCost) {
+ 
+      connection.query("UPDATE products SET stock = ? WHERE items_id = ?", [updatedStock, ID], function(err, res){
+        // if(err) throw err;
+        console.log(res);
+           console.log("Good news your order is in stock!");
+        console.log("Your total cost for " + productName + " is " + totalCost + " Thank you!");
+       
       });
-  };
+ 
+  }
 
--- seeds.sql
+++ seeds.sql
@@ -1,23 +1,39 @@
 INSERT INTO products(products_name, departments_name, price, stock)
 
-VALUES('tshirt', 'clothing', 15, 600);
+VALUES('tshirt', 'clothing', 15, 35);
 
 INSERT INTO products(products_name, departments_name, price, stock)
 
-VALUES(cologne', 'beauty', 20, 200);
+VALUES('watch', 'jewelry', 15, 35);
 
+INSERT INTO products(products_name, departments_name, price,stock)
 
-
-
-
-
-
-
-
-
-
-
+VALUES('headphone', 'electornic', 15, 35);
 
 INSERT INTO products(products_name, departments_name, price, stock)
 
-VALUES('kitchen', 'knifes', 5, 40);
+VALUES('phones', 'electronic', 15, 35);
+
+INSERT INTO products(products_name, departments_name, price, stock)
+
+VALUES('bags', 'taravel', 25, 60);
+
+INSERT INTO products(products_name, departments_name, price,stock)
+
+VALUES('rayban', 'sunglasses', 15, 80);
+
+INSERT INTO products(products_name, departments_name, price, stock)
+
+VALUES('Wallets', 'purses', 15, 600);
+
+INSERT INTO products(products_name, departments_name, price, stock)
+
+VALUES('Speaker', 'electornic', 49, 1000);
+
+INSERT INTO products(products_name, departments_name, price, stock)
+
+VALUES('accessories', 'Necklaces', 500, 800);
+
+INSERT INTO products(products_name, departments_name, price, stock)
+
+VALUES('Furniture', 'pillowcases', 4, 30);
